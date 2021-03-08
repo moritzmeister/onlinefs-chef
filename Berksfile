f36524e6021f4cabe39c972efd0e3d4945dc1547
Fabio Buso <buso.fabio@gmail.com>
2021-03-09 00:58:33 +0100

    Add correct dependencies

--- Berksfile
+++ Berksfile
@@ -1,3 +1,8 @@
 source "https://supermarket.chef.io"
 
 metadata
+
+cookbook 'kagent', github: "logicalclocks/kagent-chef", branch: "master"
+cookbook 'ndb', github: "logicalclocks/ndb-chef", branch: "master"
+cookbook 'consul', github: "logicalclocks/conda-chef", branch: "master"
+cookbook 'kkafka', github: "logicalclocks/kafka-cookbook", branch: "master"
--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,14 @@
 name              "onlinefs"
 maintainer        "Logical Clocks"
-maintainer_email  'info@logicalclocks.comm'
+maintainer_email  'info@logicalclocks.com'
 license           'GPLv3'
 description       'Installs/Configures the Hopsworks online feature store service'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "2.0.0"
+version           "2.2.0"
 
 recipe "onlinefs::default", "Configures the Hopsworks online feature store service"
+
+depends 'kkafka'
+depends 'ndb'
+depends 'kagent'
+depends 'consul'
