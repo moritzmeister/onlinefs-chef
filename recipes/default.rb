dbea5387209dfddf5a96a01ae4f8e391d84de50e
moritzmeister <meister.mo@gmail.com>
2021-03-23 15:25:43 +0100

    get api-key

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,7 @@ end
 
 user node['onlinefs']['user'] do
   home node['onlinefs']['user-home']
-  gid node['onlinefs']['group'] 
+  gid node['onlinefs']['group']
   action :create
   shell "/bin/nologin"
   manage_home true
@@ -15,9 +15,9 @@ user node['onlinefs']['user'] do
   not_if { node['install']['external_users'].casecmp("true") == 0 }
 end
 
-# Create the directories for configuration files and logs 
+# Create the directories for configuration files and logs
 ['home', 'etc', 'logs'].each {|dir|
-   directory node['onlinefs'][dir] do 
+   directory node['onlinefs'][dir] do
      owner node['onlinefs']['user']
      group node['onlinefs']['group']
      mode "0750"
@@ -34,26 +34,91 @@ kagent_hopsify "Generate x.509" do
   not_if { node["kagent"]["enabled"] == "false" }
 end
 
-# Generate an API key 
-api_key = ""
+# Generate an API key
+ruby_block 'fetch-api-key' do
+  block do
+    require 'net/https'
+    require 'http-cookie'
+    require 'json'
 
-# Template the configuration file 
+    hopsworks_fqdn = consul_helper.get_service_fqdn("hopsworks.glassfish")
+    hopsworks_endpoint = "https://#{hopsworks_fqdn}:#{node['hopsworks']['internal']['port']}"
+    url = URI.parse("#{hopsworks_endpoint}/hopsworks-api/api/auth/service")
+    api_key_url = URI.parse("#{hopsworks_endpoint}/hopsworks-api/api/users/apiKey")
+
+    params =  {
+      :email => node['onlinefs']['hopsworks']['email'],
+      :password => node['onlinefs']['hopsworks']["password"]
+    }
+
+    api_key_params = {
+      :name => "onlinefs",
+      # check with fabio which scopes are needed
+      :scope => "FEATURESTORE,KAFKA,PROJECT"
+    }
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.read_timeout = 120
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    jar = ::HTTP::CookieJar.new
+
+    http.start do |connection|
+
+      request = Net::HTTP::Post.new(url)
+      request.set_form_data(params, '&')
+      response = connection.request(request)
+
+      if( response.is_a?( Net::HTTPSuccess ) )
+          # your request was successful
+          puts "The Response -> #{response.body}"
+
+          response.get_fields('Set-Cookie').each do |value|
+            jar.parse(value, url)
+          end
+
+          request = Net::HTTP::Post.new(api_key_url)
+          request.set_form_data(api_key_params, '&')
+          request['Content-Type'] = "application/json"
+          request['Cookie'] = ::HTTP::Cookie.cookie_value(jar.cookies(api_key_url))
+          request['Authorization'] = response['Authorization']
+          response = connection.request(request)
+
+          if ( response.is_a? (Net::HTTPSuccess))
+            json_response = ::JSON.parse(response.body)
+            api_key = json_response['signedCert']
+          else
+            puts response.body
+            raise "Error creating onlinefs api-key"
+          end
+      else
+          puts response.body
+          raise "Error logging in"
+      end
+    end
+  end
+end
+
+# Template the configuration file
 kafka_fqdn = consul_helper.get_service_fqdn("broker.kafka")
 mgm_fqdn = consul_helper.get_service_fqdn("mgm.rondb")
 template "#{node['onlinefs']['etc']}/onlinefs-site.xml" do
-  source "onlinefs-site.xml.erb" 
+  source "onlinefs-site.xml.erb"
   owner node['onlinefs']['user']
   group node['onlinefs']['group']
   mode 0750
-  variables({
-              :kafka_fqdn => kafka_fqdn,
-              :mgm_fqdn => mgm_fqdn,
-              :api_key => api_key
-           })
+  variables( lazy {
+    {
+      :kafka_fqdn => kafka_fqdn,
+      :mgm_fqdn => mgm_fqdn,
+      :api_key => api_key
+    }
+  })
 end
 
 template "#{node['onlinefs']['etc']}/log4j.properties" do
-  source "log4j.properties.erb" 
+  source "log4j.properties.erb"
   owner node['onlinefs']['user']
   group node['onlinefs']['group']
   mode 0750
@@ -91,7 +156,7 @@ service service_name do
   action :nothing
 end
 
-template systemd_script do 
+template systemd_script do
   source "#{service_name}.service.erb"
   owner "root"
   group "root"
